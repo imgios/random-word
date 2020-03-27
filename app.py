2cc469381c380d09ac5eeb813d4f6fd744f63e81
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-03-27 12:39:27 +0000

    Modify API endpoint

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def randomWord(word_length=8):
 def home():
 	return "<h1>A basic API made in Python</h1><p>This website a basic API prototype build with Python3 and Flask!</p>"
 	
-@app.route('/word', methods=['GET'])
+@app.route('/api/word', methods=['GET'])
 def api_word():
 	if 'length' in request.args:
 		lenght = int(request.args['length'])
